1bfba2521672a6c1f0cf8ad8acbd1b0c497833e6
daeks <daeks@gmx.de>
2017-02-08 21:11:12 +0100

    some fixes

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,9 +6,9 @@ def Start():
 def ValidatePrefs():
   pass
 
-class ExportAgent(Agent.Movies):
+class WebhookAgent(Agent.Movies):
 
-  name = 'Export Agent'
+  name = 'Webhook Metadata Agent'
   languages = [Locale.Language.NoLanguage]
   primary_provider = False
   contributes_to = ['com.plexapp.agents.none']
@@ -20,41 +20,47 @@ class ExportAgent(Agent.Movies):
   def update(self, metadata, media, lang):
   
     if Prefs['webhook']:
-      part = media.items[0].parts[0]
-      path = os.path.dirname(part.file)
-      (root_file, ext) = os.path.splitext(os.path.basename(part.file))
+      if Prefs['combined']:
+        Log('Loading data of contributer combined')
+        self.hook(media, metadata.contribution('_combined'))
 
-      for contributor in metadata.contributors:
-        if contributor.startswith('com.'):
-          Log('Loading data of contributer %s' % contributor)
-          data = metadata.contribution(contributor)
-          
-          output = {}
-          output['event'] = 'metadata'
-          output['provider'] = str(data.provider)
-          output['id'] = data.id
-          output['guid'] = data.guid
-          output['root_file'] = root_file
-          output['ext'] = ext
-          
-          if data.title:
-            output['title'] = str(data.title.encode('utf-8'))
-          
-          if data.summary:
-            output['summary'] = str(data.summary.encode('utf-8'))
+      if Prefs['contributors']:
+        for contributor in metadata.contributors:
+          if contributor.startswith('com.'):
+            Log('Loading data of contributer %s' % contributor)
+            self.hook(media, metadata.contribution(contributor))
             
-          if data.rating:
-            output['rating'] = data.rating
-            
-          if data.title_sort:
-            output['title_sort'] = str(data.title_sort.encode('utf-8'))
-         
-          jdata = JSON.StringFromObject(output)
-          
-          Log('Parsed metadata to JSON String %s' % jdata)
-          Log('Sending payload to %s' % Prefs['webhook'])
-          post_values = {'payload': jdata}
-          result = HTTP.Request(Prefs['webhook'], values=post_values)
+  def hook(self, media, data):
+    part = media.items[0].parts[0]
+    path = os.path.dirname(part.file)
+    (root_file, ext) = os.path.splitext(os.path.basename(part.file))
+  
+    output = {}
+    output['event'] = 'metadata.update'
+    output['provider'] = str(data.provider)
+    output['id'] = data.id
+    output['guid'] = data.guid
+    output['root_file'] = root_file
+    output['ext'] = ext
+    
+    if data.title:
+      output['title'] = str(data.title.encode('utf-8'))
+    
+    if data.summary:
+      output['summary'] = str(data.summary.encode('utf-8'))
+      
+    if data.rating:
+      output['rating'] = data.rating
+      
+    if data.title_sort:
+      output['title_sort'] = str(data.title_sort.encode('utf-8'))
+   
+    jdata = JSON.StringFromObject(output)
+    
+    Log('Parsed metadata to JSON String %s' % jdata)
+    Log('Sending payload to %s' % Prefs['webhook'])
+    post_values = {'payload': jdata}
+    result = HTTP.Request(Prefs['webhook'], values=post_values)
 
   def dump(self, obj):
     for attr in dir(obj):
